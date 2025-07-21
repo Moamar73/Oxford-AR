d762f1363369b063b5fcb33ca3e936adbcc08a0b
Moamar73 <sahafy@hotmail.com>
2025-07-21 23:21:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  output: 'standalone',
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
     formats: ['image/webp', 'image/avif'],
